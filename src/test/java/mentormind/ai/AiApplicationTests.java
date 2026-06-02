79ff6e0bbc4843bc5b00aae50596c15ff911f6e3
felipematheus1337 <lipehbr@gmail.com>
2026-06-02 19:36:57 -0300

    feature: project created.

---
+++ src/test/java/mentormind/ai/AiApplicationTests.java
@@ -0,0 +1,13 @@
+package mentormind.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
